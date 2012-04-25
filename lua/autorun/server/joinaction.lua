93378a5fef85df1aaad2d369f3f60cc83ba3b172
gdeleeuw@leeuwit.nl <gdeleeuw@leeuwit.nl@0b51b216-e7ea-11de-8dea-4d08a2fd72f5>
2012-04-25 21:12:06 +0000

    Removed SteamID check

--- lua/autorun/server/joinaction.lua
+++ lua/autorun/server/joinaction.lua
@@ -1,11 +1,9 @@
--- resource.AddFile( "sound/custom/lion_roar.mp3" )
+resource.AddFile( "sound/custom/lion_roar.mp3" )
 
 function lsJoinAction( ply )
-	-- if tostring(ply:SteamID()) == "STEAM_0:1:15924980" then
-		-- ply:EmitSound("custom/lion_roar.mp3", 500, 100)
-	-- end
 	if ply:IsSuperAdmin() then
-		ply:ChatPrint("Welcome " .. ply:Name() .. ". Enjoy your ub3rl33t superadmin p0w4zzz.") 
+   ply:ChatPrint("Welcome " .. ply:Name() .. ". Enjoy your ub3rl33t superadmin p0w4zzz.") 
+   ply:EmitSound("custom/lion_roar.mp3", 500, 100)
 	end
 end
 hook.Add( "PlayerInitialSpawn", "lsPlayerInitialSpawn", lsJoinAction )
